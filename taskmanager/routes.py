170187d3466eff07efc6b059772397a13c06da6f
Ekta Mehra <mehra.ekta95@gmail.com>
2024-12-30 12:17:16 +0000

    Add categories link to nav bar and in routes.py

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,4 +5,9 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
